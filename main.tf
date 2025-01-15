f1c50acaddedbe7c1ee20e92cbf8dc291e6bfd41
Harsh Singh <ankitharsh7542@gmail.com>
2025-01-15 10:43:40 +0530

    ec2_instance_web

--- main.tf
+++ main.tf
@@ -1,1 +1,27 @@
-#i am good
+# first resource block by harsh
+# Fetch the latest Ubuntu AMI
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+# Create an EC2 instance
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "devops_firstec2"
+  }
+}
